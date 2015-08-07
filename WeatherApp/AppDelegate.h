214b25fdd111803265c378536ff4c1b8b047502a
User4 <user4@User4s-iMac.local>
2015-08-07 12:48:38 +0300

    Initial Commit

---
+++ WeatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherApp
+//
+//  Created by User4 on 07.08.15.
+//  Copyright (c) 2015 Dmitry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WeatherApp
+//
+//  Created by User4 on 07.08.15.
+//  Copyright (c) 2015 Dmitry. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
